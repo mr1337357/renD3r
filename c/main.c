b7bcb82770c1c6f814e1b941aa0d4684ee9f47fa
misspapaya <missp@pa.ya>
2019-10-09 18:08:11 -0700

    idk not much

--- c/main.c
+++ c/main.c
@@ -1,6 +1,47 @@
-#include <SDL.h>
+#include "SDL.h"
+#include <stdio.h>
 
-int main(int argc,char **argv)
-{
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;                    // Declare a pointer
+    SDL_Surface *surface;
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    surface = SDL_GetWindowSurface( window );
+
+    //Fill the surface white
+    SDL_FillRect( surface, NULL, SDL_MapRGB( surface->format, 0xFF, 0xFF, 0xFF ) );
+    
+    //Update the surface
+    SDL_UpdateWindowSurface( window );
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
     return 0;
 }
+
