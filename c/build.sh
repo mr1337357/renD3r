af58d8ecc2a7ab88f625fdae0c8176261856d76f
misspapaya <missp@pa.ya>
2019-10-11 13:58:44 -0700

    rendering points

--- c/build.sh
+++ c/build.sh
@@ -1,5 +1,6 @@
 CFLAGS=$(pkg-config --cflags sdl2)
 LDFLAGS=$(pkg-config --libs sdl2)
 gcc -c main.c -o main.o $CFLAGS
-gcc main.o -o main $LDFLAGS
+gcc -c vector.c -o vector.o $CFLAGS
+gcc main.o vector.o -o main $LDFLAGS
 gcc sdltest.c -o sdltest $CFLAGS $LDFLAGS
--- c/vector.c
+++ c/vector.c
@@ -1,9 +1,13 @@
 #include "vector.h"
+#include <stdio.h>
 
 vector4 *transform(matrix4x4 *m,vector4 *vin,vector4 *vout)
 {
     int i;
     vout->x = 0;
+    vout->y = 0;
+    vout->z = 0;
+    vout->w = 0;
     for(i=0;i<4;i++)
     {
         vout->x += vin->v[i]*m->x[i];
@@ -12,4 +16,43 @@ vector4 *transform(matrix4x4 *m,vector4 *vin,vector4 *vout)
         vout->w += vin->v[i]*m->w[i];
     }
     return vout;
+}
+
+vector4 *project(vector4 *vin)
+{
+    vin->x/=vin->z;
+    vin->y/=vin->z;
+    return vin;
+}
+
+void dump_vector(vector4 *vin)
+{
+    int i;
+    printf("VECTOR: %f %f %f %f\n",vin->x,vin->y,vin->z,vin->w);
+}
+
+void dump_matrix(matrix4x4 *m)
+{
+    int i;
+    printf("MATRIX:\n");
+    for(i=0;i<4;i++)
+    {
+        printf("%f ",m->x[i]);
+    }
+    printf("\n");
+    for(i=0;i<4;i++)
+    {
+        printf("%f ",m->y[i]);
+    }
+    printf("\n");
+    for(i=0;i<4;i++)
+    {
+        printf("%f ",m->z[i]);
+    }
+    printf("\n");
+    for(i=0;i<4;i++)
+    {
+        printf("%f ",m->w[i]);
+    }
+    printf("\n");
 }
